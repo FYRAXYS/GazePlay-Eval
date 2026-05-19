c8f3e9d061b8dc3d75eb0fb3d37a75cb408de757
FYRAXYS <fyraxys2006@gmail.com>
2026-05-19 15:26:05 +0200

    Rétablissement de l'import du MenuComponent qui est utilisé dans le fichier HTML

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {ThemeService} from '../../services/theme/theme.service';
+import {MenuComponent} from '../menu/menu.component';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './navbar.component.html',
   standalone: true,
   styleUrl: './navbar.component.css'
