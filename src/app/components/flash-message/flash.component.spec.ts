873f44775bef76b1af559a232e1ee51a21c74f0c
FYRAXYS <fyraxys2006@gmail.com>
2026-05-22 09:48:50 +0200

    Ajouts de commentaires et refactoring de flash.component.spec.ts.

--- src/app/components/flash-message/flash.component.spec.ts
+++ src/app/components/flash-message/flash.component.spec.ts
@@ -22,28 +22,34 @@ describe('FlashComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('mapType(\'success\') → \'success\'', () => {
-    expect(component.mapType('success')).toEqual('success');
+  // Tests portant sur les dictionnaires utilisés pour le style des flash-messages
+  describe('mapType', () => {
+    it('mapType(\'success\') → \'success\'', () => {
+      expect(component.mapType('success')).toEqual('success');
+    });
+
+    it('mapType(\'error\') → \'danger\'', () => {
+      expect(component.mapType('error')).toEqual('danger');
+    });
+
+    it('mapType(\'unknown\') → \'info\'', () => {
+      expect(component.mapType('unknown')).toEqual('info');
+    });
   });
 
-  it('mapType(\'error\') → \'danger\'', () => {
-    expect(component.mapType('error')).toEqual('danger');
-  });
+  describe('mapIcon', () => {
 
-  it('mapType(\'unknown\') → \'info\'', () => {
-    expect(component.mapType('unknown')).toEqual('info');
-  });
+    it('mapIcon(\'success\') → \'bi-check-circle\'', () => {
+      expect(component.mapIcon('success')).toEqual('bi-check-circle');
+    });
 
-  it('mapIcon(\'success\') → \'bi-check-circle\'', () => {
-    expect(component.mapIcon('success')).toEqual('bi-check-circle');
-  });
+    it('mapIcon(\'error\') → \'bi-x-circle\'', () => {
+      expect(component.mapIcon('error')).toEqual('bi-x-circle');
+    });
 
-  it('mapIcon(\'error\') → \'bi-x-circle\'', () => {
-    expect(component.mapIcon('error')).toEqual('bi-x-circle');
-  });
-
-  it('mapIcon(\'unknown\') → \'bi-info-circle\'', () => {
-    expect(component.mapIcon('unknown')).toEqual('bi-info-circle');
+    it('mapIcon(\'unknown\') → \'bi-info-circle\'', () => {
+      expect(component.mapIcon('unknown')).toEqual('bi-info-circle');
+    });
   });
 
 });
