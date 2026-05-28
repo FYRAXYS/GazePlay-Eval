cd6f76c728e059fbb6bf8c9d79b18e8cff7eb55a
FYRAXYS <fyraxys2006@gmail.com>
2026-05-28 09:36:42 +0200

    Initialisation du guide utilisateur.

---
+++ src/app/pages/guide/guide.component.ts
@@ -0,0 +1,12 @@
+import {RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-guide',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './guide.component.html',
+  styleUrl: './guide.component.css',
+
+})
+export class GuideComponent {}
