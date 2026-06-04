5a0a7b131b901930f67f7e074b7d36c2b8ac7d6d
FYRAXYS <fyraxys2006@gmail.com>
2026-06-04 10:31:56 +0200

    Ajout d'une sauvegarde automatique à chaque ajout de nouvel écran.

--- src/app/pages/create-eval/create-eval.component.ts
+++ src/app/pages/create-eval/create-eval.component.ts
@@ -10,6 +10,7 @@ import {
 } from '../../shared/screenModel';
 import {ModifyScreenComponent} from '../../components/modify-screen/modify-screen.component';
 import {UpdateScreensService} from '../../services/updateScreens/update-screens.service';
+import {AutoSaveService} from '../../services/auto-save/auto-save.service';
 
 @Component({
   selector: 'app-create-eval',
@@ -29,7 +30,10 @@ export class CreateEvalComponent implements OnInit{
   idScreen: number = 1;
   editNameScreenDisable: boolean = true;
 
-  constructor(private router: Router, private saveService: SaveService, private updateScreenService: UpdateScreensService) {
+  constructor(private router: Router,
+              private saveService: SaveService,
+              private updateScreenService: UpdateScreensService,
+              private autoSaveService: AutoSaveService) {
   }
 
   ngOnInit(): void {
@@ -58,6 +62,8 @@ export class CreateEvalComponent implements OnInit{
     this.selectedScreen = newScreen;
     this.listScreens.push(newScreen);
     this.indexSelectedScreen = this.listScreens.length - 1;
+
+    this.autoSaveService.autoSave('create-eval');
   }
 
   selectScreen(screen: screenTypeModel, index: number) {
